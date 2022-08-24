0fd153b5f7855282f202612e6f5fe970f5b62c1b
dhawal07 <dhawalshende7@gmail.com>
2022-08-24 20:49:10 +0530

    dhawal day2

---
+++ scripts/series.js
@@ -0,0 +1,134 @@
+let international_tours = 
+[
+    "England v South Africa" ,
+    "Zimbabwe v India" ,
+    "West Indies v New Zealand" ,
+    "Netherlands v Pakistan" ,
+    "Australia v Zimbabwe" ,
+    "BAN-A in West Indies" ,
+    "SL U-19s in ENG" ,
+    "IRE-W in NL"
+]
+
+let international_tournaments =
+[
+    "Asia Cup Qualifier" ,
+    "Asia Cup 2022" ,
+    "CWC Challenge Group A" ,
+    "CWC Challenge Group B"
+]
+
+let icc_tournaments = 
+[
+    "ICC Women's Championship" ,
+    "Men's Cricket World Cup League 2" ,
+    "CWC Challenge League Group A" ,
+    "CWC Challenge League Group B" ,
+    "ICC CWC Super League" ,
+    "ICC World Test Championship" 
+]
+
+let t20_tournaments =
+[
+    "Asia Cup 2022" ,
+    "SLC T20" ,
+    "Maharaja T20" ,
+    "6IXTY (W)" , 
+    "6IXTY (M)" ,
+    "T20 QUAD (in UAE)" ,
+    "Namibia T20" 
+]
+
+let associate_cricket =
+[
+    "Kenya v Nepal" ,
+    "Bahrain v Kuwait" ,
+    "ITA-W in Austria" ,
+    "Continental Cup"
+]
+
+let australia_domestic = 
+[
+    "Sheffield Shield" ,
+    "The Marsh Cup" ,
+    "BBL 2022" ,
+    "WBBL 2022" ,
+    "WNC League" 
+]
+
+let england_domestic =
+[
+    "County Div1" ,
+    "County Div2" ,
+    "100 (Men's)" ,
+    "100 (Women's)" ,
+    "RL Cup" ,
+    "Vitality Blast" ,
+    "RHF Cup" 
+]
+
+let newzealand_domestic = 
+[
+    "Plunket Shield" ,
+    "Ford Trophy" ,
+    "Women's 1-Day"
+]
+
+let westindies_domestic = 
+[
+    "6IXTY (W)" ,
+    "6IXTY (M)" ,
+    "CPL 2022" ,
+    "WCPL 2022"
+]
+
+let future_int_tours = 
+[
+    "NZ-A in India, Sep 2022" ,
+    "India Women tour of England, Sep 2022" ,
+    "New Zealand tour of Australia, Sep 2022" ,
+    "England tour of Pakistan, Sep 2022" ,
+    "Australia tour of India, Sep 2022" ,
+    "South Africa tour of India, Sep 2022" ,
+    "New Zealand T20I Tri-Series, Oct 2022" ,
+    "West Indies tour of Australia, Oct 2022" ,
+    "England tour of Australia, Oct 2022" ,
+    "India tour of New Zealand, Nov 2022" ,
+    "South Africa tour of Australia, Dec 2022" ,
+    "Bangladesh Women tour of New Zealand, Dec 2022" ,
+    "England tour of New Zealand, Feb 2023" ,
+    "Sri Lanka tour of New Zealand, Mar 2023" 
+]
+
+let recent_int_tours =
+[
+    "Zimbabwe v Bangladesh" ,
+    "Ireland v Afghanistan" ,
+    "West Indies v India" ,
+    "Sri Lanka v Pakistan" ,
+    "Netherlands v New Zealand" ,
+    "SA-W in England" ,
+    "Scotland v New Zealand" ,
+    "IRE Tri-Series (Women)" ,
+    "Ireland v New Zealand" ,
+    "England v India" ,
+    "West Indies v Bangladesh" ,
+    "Sri Lanka v Australia" ,
+    "IND-W in Sri Lanka" ,
+    "Ireland v India" ,
+    "England v India (5th Test)" ,
+    "Netherlands v England" ,
+    "England v New Zealand" ,
+    "AUS-A in Sri Lanka" ,
+    "India v South Africa"
+]
+
+let recent_associate_cricket = 
+[
+    "Canada v Nepal" ,
+    "Austria v Hungary" ,
+    "Sofia T20" ,
+    "Finland v Estonia" ,
+    "Bulgaria v Serbia" ,
+    "Namibia Tri-Series" ,
+]
---
+++ scripts/teams.js
@@ -0,0 +1,145 @@
+let int_teams = [
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/321000/321005.png" ,
+        name : "Afghanistan"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/340400/340493.png" ,
+        name : "Australia"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/341400/341456.png" ,
+        name : "Bangladesh"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313114.logo.png" ,
+        name : "England"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313128.logo.png" ,
+        name : "India"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313149.logo.png" ,
+        name : "Ireland"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/340500/340505.png" ,
+        name : "New Zealand"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313129.logo.png" ,
+        name : "Pakistan"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313150.logo.png" ,
+        name : "Scotland"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313125.logo.png" ,
+        name : "South Africa"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/340000/340047.png" ,
+        name : "Sri Lanka"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313100/313147.logo.png" ,
+        name : "United Arab Emirates"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/317600/317615.png" ,
+        name : "West Indies"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/340500/340503.png" ,
+        name : "Zimbabwe"
+    } 
+]
+
+let womens_team = [
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313200/313225.logo.png" ,
+        name : "Australia Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313401.logo.png" ,
+        name : "Bangladesh Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313200/313261.logo.png" ,
+        name : "England Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313300/313309.logo.png" ,
+        name : "India Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313300/313335.logo.png" ,
+        name : "New Zealand Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313300/313341.logo.png" ,
+        name : "Pakistan Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313300/313348.logo.png" ,
+        name : "South Africa Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/340000/340047.png" ,
+        name : "Sri Lanka Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313402.logo.png" ,
+        name : "Thailand Women"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/317600/317615.png" ,
+        name : "West Indies Women"
+    } ,
+]
+
+let ipl = 
+[
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313421.logo.png" ,
+        name : "Chennai Super Kings"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313422.logo.png" ,
+        name : "Delhi Capitals"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/334700/334707.png" ,
+        name : "Gujarat Titans"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313419.logo.png" ,
+        name : "Kolkata Knight Riders"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/333800/333885.png" ,
+        name : "Lucknow Super Giants"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/344000/344062.png" ,
+        name : "Mumbai Indians"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/317000/317003.png" ,
+        name : "Punjab Kings"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313423.logo.png" ,
+        name : "Rajasthan Royals"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313418.logo.png" ,
+        name : "Royal Challengers Bangalore"
+    } ,
+    {
+        img : "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/313400/313480.logo.png" ,
+        name : "Sunrisers Hyderabad"
+    } ,
+]
