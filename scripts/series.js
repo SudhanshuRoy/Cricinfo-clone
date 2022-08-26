2e7181ffd1783c343315b88b4bcd29d862c6bc2a
dhawal07 <dhawalshende7@gmail.com>
2022-08-26 20:41:30 +0530

    day4

--- scripts/series.js
+++ scripts/series.js
@@ -1,86 +1,88 @@
-let international_tours = 
-[
-    "England v South Africa" ,
-    "Zimbabwe v India" ,
-    "West Indies v New Zealand" ,
-    "Netherlands v Pakistan" ,
-    "Australia v Zimbabwe" ,
-    "BAN-A in West Indies" ,
-    "SL U-19s in ENG" ,
-    "IRE-W in NL"
-]
 
-let international_tournaments =
-[
-    "Asia Cup Qualifier" ,
-    "Asia Cup 2022" ,
-    "CWC Challenge Group A" ,
-    "CWC Challenge Group B"
-]
 
-let icc_tournaments = 
-[
-    "ICC Women's Championship" ,
-    "Men's Cricket World Cup League 2" ,
-    "CWC Challenge League Group A" ,
-    "CWC Challenge League Group B" ,
-    "ICC CWC Super League" ,
-    "ICC World Test Championship" 
-]
+// let international_tours = 
+// [
+//     "England v South Africa" ,
+//     "Zimbabwe v India" ,
+//     "West Indies v New Zealand" ,
+//     "Netherlands v Pakistan" ,
+//     "Australia v Zimbabwe" ,
+//     "BAN-A in West Indies" ,
+//     "SL U-19s in ENG" ,
+//     "IRE-W in NL"
+// ]
 
-let t20_tournaments =
-[
-    "Asia Cup 2022" ,
-    "SLC T20" ,
-    "Maharaja T20" ,
-    "6IXTY (W)" , 
-    "6IXTY (M)" ,
-    "T20 QUAD (in UAE)" ,
-    "Namibia T20" 
-]
+// let international_tournaments =
+// [
+//     "Asia Cup Qualifier" ,
+//     "Asia Cup 2022" ,
+//     "CWC Challenge Group A" ,
+//     "CWC Challenge Group B"
+// ]
 
-let associate_cricket =
-[
-    "Kenya v Nepal" ,
-    "Bahrain v Kuwait" ,
-    "ITA-W in Austria" ,
-    "Continental Cup"
-]
+// let icc_tournaments = 
+// [
+//     "ICC Women's Championship" ,
+//     "Men's Cricket World Cup League 2" ,
+//     "CWC Challenge League Group A" ,
+//     "CWC Challenge League Group B" ,
+//     "ICC CWC Super League" ,
+//     "ICC World Test Championship" 
+// ]
 
-let australia_domestic = 
-[
-    "Sheffield Shield" ,
-    "The Marsh Cup" ,
-    "BBL 2022" ,
-    "WBBL 2022" ,
-    "WNC League" 
-]
+// let t20_tournaments =
+// [
+//     "Asia Cup 2022" ,
+//     "SLC T20" ,
+//     "Maharaja T20" ,
+//     "6IXTY (W)" , 
+//     "6IXTY (M)" ,
+//     "T20 QUAD (in UAE)" ,
+//     "Namibia T20" 
+// ]
 
-let england_domestic =
-[
-    "County Div1" ,
-    "County Div2" ,
-    "100 (Men's)" ,
-    "100 (Women's)" ,
-    "RL Cup" ,
-    "Vitality Blast" ,
-    "RHF Cup" 
-]
+// let associate_cricket =
+// [
+//     "Kenya v Nepal" ,
+//     "Bahrain v Kuwait" ,
+//     "ITA-W in Austria" ,
+//     "Continental Cup"
+// ]
 
-let newzealand_domestic = 
-[
-    "Plunket Shield" ,
-    "Ford Trophy" ,
-    "Women's 1-Day"
-]
+// let australia_domestic = 
+// [
+//     "Sheffield Shield" ,
+//     "The Marsh Cup" ,
+//     "BBL 2022" ,
+//     "WBBL 2022" ,
+//     "WNC League" 
+// ]
 
-let westindies_domestic = 
-[
-    "6IXTY (W)" ,
-    "6IXTY (M)" ,
-    "CPL 2022" ,
-    "WCPL 2022"
-]
+// let england_domestic =
+// [
+//     "County Div1" ,
+//     "County Div2" ,
+//     "100 (Men's)" ,
+//     "100 (Women's)" ,
+//     "RL Cup" ,
+//     "Vitality Blast" ,
+//     "RHF Cup" 
+// ]
+
+// let newzealand_domestic = 
+// [
+//     "Plunket Shield" ,
+//     "Ford Trophy" ,
+//     "Women's 1-Day"
+// ]
+
+// let westindies_domestic = 
+// [
+//     "6IXTY (W)" ,
+//     "6IXTY (M)" ,
+//     "CPL 2022" ,
+//     "WCPL 2022"
+// ]
 
 let future_int_tours = 
 [
@@ -132,3 +134,74 @@ let recent_associate_cricket =
     "Bulgaria v Serbia" ,
     "Namibia Tri-Series" ,
 ]
+
+function current(){
+    window.location.href = "series.html"
+}
+
+function future(){
+
+    let change = document.querySelector("#names")
+    change.innerHTML = null;
+    change.style.display = "block";
+
+    let title = document.createElement("h2");
+    title.innerText = "INTERNATIONAL TOURS"
+    change.append(title);
+
+    let div = document.createElement("div");
+
+    future_int_tours.forEach(function(elem){
+
+        let x = document.createElement("h4");
+        x.innerText = elem;
+
+        div.append(x);
+    })
+
+    change.append(div)
+}
+
+
+
+function recent(){
+    let change = document.querySelector("#names")
+    change.innerHTML = null;
+    change.style.display = "block";
+
+    let title = document.createElement("h2");
+    title.innerText = "INTERNATIONAL TOURS"
+    change.append(title);
+
+    let div = document.createElement("div");
+    div.style.marginBottom = "50px";
+
+    recent_int_tours.forEach(function(elem){
+
+        let x = document.createElement("h4");
+        x.innerText = elem;
+
+        div.append(x);
+    })
+
+    change.append(div)
+
+
+
+    let title2 = document.createElement("h2");
+    title2.innerText = "ASSOCIATE CRICKET"
+    change.append(title2);
+    title2.style.marginTop = "30px"
+
+    let div2 = document.createElement("div");
+
+    recent_associate_cricket.forEach(function(elem){
+
+        let x = document.createElement("h4");
+        x.innerText = elem;
+
+        div2.append(x);
+    })
+
+    change.append(div2)
+}
